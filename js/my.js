cdf9659dbbaa147084b8f47ba78e51f35641f8a8
McsCasicas <slashlicks07@gmail.com>
2021-01-31 23:23:01 +0800

    Hi

---
+++ js/my.js
@@ -0,0 +1,16 @@
+let year = 2001;
+let month = "April";
+let day = 7;
+
+console.log(month + day + year)
+
+let gwapo = "macky";
+let sino = "gwapo";
+console.log(sino + gwapo)
+
+
+let months = ["january" , "february", "march", "april","may","june","july", "august", "september", "october", "november", "december"]
+console.log(months)
+
+
+
